55a731505398cb45d4cfaf80b727ea273611a167
mkutu <mattkutugata@gmail.com>
2022-06-01 14:59:45 -0500

    update

--- CrSDK_v1.05.00_20211207a_Linux64PC/app/RemoteCli.cpp
+++ CrSDK_v1.05.00_20211207a_Linux64PC/app/RemoteCli.cpp
@@ -27,85 +27,69 @@ int main()
     using namespace std::chrono_literals; // ns, us, ms, s, h, etc.
     using std::chrono::system_clock;
 
-    // Make the stream's locale the same as the current global locale
-    cli::tin.imbue(std::locale());
-    cli::tout.imbue(std::locale());
-
-    cli::tout << "RemoteSampleApp v1.05.00 running...\n\n";
-
-    CrInt32u version = SDK::GetSDKVersion();
-    int major = (version & 0xFF000000) >> 24;
-    int minor = (version & 0x00FF0000) >> 16;
-    int patch = (version & 0x0000FF00) >> 8;
-
-    cli::tout << "Remote SDK version: ";
-    cli::tout << major << "." << minor << "." << std::setfill(TEXT('0')) << std::setw(2) << patch << "\n";
-
-    cli::tout << "Initialize Remote SDK...\n";
-    cli::tout << "Working directory: " << fs::current_path() << '\n';
-
     auto init_success = SDK::Init();
-    if (!init_success)
-    {
-        cli::tout << "Failed to initialize Remote SDK. Terminating.\n";
-        // cr_lib->Release();
-        SDK::Release();
-        std::exit(EXIT_FAILURE);
-    }
-    cli::tout << "Remote SDK successfully initialized.\n\n";
-
-    cli::tout << "Enumerate connected camera devices...\n";
+    // Enumerte connected cameras
     SDK::ICrEnumCameraObjectInfo *camera_list = nullptr;
-
     auto enum_status = SDK::EnumCameraObjects(&camera_list);
-    if (CR_FAILED(enum_status) || camera_list == nullptr)
-    {
-        cli::tout << "No cameras detected. Connect a camera and retry.\n";
-
-        SDK::Release();
-        std::exit(EXIT_FAILURE);
-    }
-    auto ncams = camera_list->GetCount();
-    cli::tout << "Camera enumeration successful. " << ncams << " detected.\n\n";
-
-    for (CrInt32u i = 0; i < ncams; ++i)
-    {
-        auto camera_info = camera_list->GetCameraObjectInfo(i);
-        cli::text conn_type(camera_info->GetConnectionTypeName());
-        cli::text model(camera_info->GetModel());
-        cli::text id = TEXT("");
-        if (TEXT("IP") == conn_type)
-        {
-            cli::NetworkInfo ni = cli::parse_ip_info(camera_info->GetId(), camera_info->GetIdSize());
-            id = ni.mac_address;
-        }
-        else
-            id = ((TCHAR *)camera_info->GetId());
-        cli::tout << '[' << i + 1 << "] " << model.data() << " (" << id.data() << ")\n";
-    }
-
-    cli::tsmatch smatch;
-    CrInt32u no = 1;
-
+    auto camera_info = camera_list->GetCameraObjectInfo(0);
+    //IDeviceCallback function below this line
     typedef std::shared_ptr<cli::CameraDevice> CameraDevicePtr;
     typedef std::vector<CameraDevicePtr> CameraDeviceList;
     CameraDeviceList cameraList; // all
     std::int32_t cameraNumUniq = 1;
     std::int32_t selectCamera = 1;
-
-    cli::tout << "Connect to selected camera...\n";
-    auto *camera_info = camera_list->GetCameraObjectInfo(no - 1);
-
-    cli::tout << "Create camera SDK camera callback object.\n";
+    // this is IDeviceCallback function
     CameraDevicePtr camera = CameraDevicePtr(new cli::CameraDevice(cameraNumUniq, nullptr, camera_info));
     cameraList.push_back(camera); // add 1st
-
-    sleep_for(100ns);
+    cli::tout << camera << " Camera.\n";
+    // IDK why we release the camera list here
+    camera_list->Release();
+    
+    //connect
     camera->connect(SDK::CrSdkControlMode_Remote);
     sleep_for(3s);
     camera->af_shutter();
+    sleep_for(2s);
+    // cli::tout << "Something.\n";
+
+
     cli::tout << "Release SDK resources.\n";
     SDK::Release();
     cli::tout << "Exiting application.\n";
     std::exit(EXIT_SUCCESS);
+
 }
+//     cli::tsmatch smatch;
+//     CrInt32u no = 0;
+
+
+
+
+//     cli::tsmatch smatch;
+//     CrInt32u no = 1;
+
+
+
+//     typedef std::shared_ptr<cli::CameraDevice> CameraDevicePtr;
+//     typedef std::vector<CameraDevicePtr> CameraDeviceList;
+//     CameraDeviceList cameraList; // all
+//     std::int32_t cameraNumUniq = 1;
+//     std::int32_t selectCamera = 1;
+
+//     cli::tout << "Connect to selected camera...\n";
+//     auto *camera_info = camera_list->GetCameraObjectInfo(no - 1);
+
+//     cli::tout << "Create camera SDK camera callback object.\n";
+//     CameraDevicePtr camera = CameraDevicePtr(new cli::CameraDevice(cameraNumUniq, nullptr, camera_info));
+//     cameraList.push_back(camera); // add 1st
+    
+//     sleep_for(2s);
+//     camera->connect(SDK::CrSdkControlMode_Remote);
+//     sleep_for(3s);
+//     camera->af_shutter();
+//     cli::tout << std::endl;
+//     cli::tout << "Release SDK resources.\n";
+//     SDK::Release();
+//     cli::tout << "Exiting application.\n";
+//     std::exit(EXIT_SUCCESS);
+// }
